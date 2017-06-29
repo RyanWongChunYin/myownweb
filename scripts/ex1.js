bc607db4c93408b06513dadd988eab195f6e89c3
Ryan Wong <Ryan.Wong@fdmgroup.local>
2017-06-29 18:36:59 +0800

    29/6 18:00

--- scripts/ex1.js
+++ scripts/ex1.js
@@ -29,7 +29,7 @@ function StockPriceTick(input) {
     var CNames = input;
     var flickerAPI = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22" + CNames + "%22)&env=store://datatables.org/alltableswithkeys";
     var StockTickerHTML = "";
-    
+    //http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%222800.HK%22)&env=store://datatables.org/alltableswithkeys
     var StockTickerXML = $.get(flickerAPI, function(xml) {
         $(xml).find("quote").each(function () {
             Symbol = $(this).attr("symbol");
--- scripts/scraperSQL.js
+++ scripts/scraperSQL.js
@@ -1,4 +1,5 @@
-function getCSV(){
+
+function getSQL(){
 
 	var input = document.getElementById("sym").value;
 
@@ -17,6 +18,7 @@ function getCSV(){
 	/*
 	var url = "https://www.google.com/finance/historical?q=HKG%3A2800&startdate=Jun+29%2C+2016&num=200&ei=KMZTWaHOGtyS0wTtwLK4BQ";
 	*/
+	
 	var url = "https://www.google.com/finance/historical?q=HKG%3A2800&startdate=Jun+29%2C+2016&num=200&ei=KMZTWaHOGtyS0wTtwLK4BQ";
 	var trtag = "tr";
 	var thtag = "th";
@@ -24,6 +26,13 @@ function getCSV(){
 	var res;
 	var text;
 	var outputHTML = "";
+	var tableName;
+	if(document.getElementById("tablename").value == ""){
+		tableName = "historical_price";
+	}else{
+		tableName = document.getElementById("tablename").value;
+	}
+
 	var yql ="https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20htmlstring%20where%20url%3D%27https%3A%2F%2Fwww.google.com%2Ffinance%2Fhistorical%3Fq%3DHKG%253A"+input+"%26startdate%3DJun%2B29%252C%2B2016%26num%3D200%26ei%3DKMZTWaHOGtyS0wTtwLK4BQ%27%20and%20xpath%3D%27%2F%2F%20tr%27&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
 	var StockTickerXML = $.get(yql, function(xml) {
 		result = $(xml).find("result").text();
@@ -40,11 +49,12 @@ function getCSV(){
 		var len = res.length;
 		res = res.slice(5,len);
 		console.log(res);
-		for(i =0;i<res.length;i++){
+		outputHTML = outputHTML +"<span>CREATE TABLE CUSTOMERS(STOCK INT NOT NULL,DATE DATE NOT NULL,OPEN DECIMAL (18, 2),HIGH DECIMAL (18, 2), LOW  DECIMAL (18, 2), CLOSE DECIMAL (18, 2), VOLUME DECIMAL INT, PRIMARY KEY (STOCK,DATE));</span><br><br>"
+		for(i =6;i<res.length;i++){
 			if(i%6 == 0){
-				outputHTML = outputHTML + "<span>"+res[i]+",";
+				outputHTML = outputHTML + "<span>INSERT INTO "+tableName+" (STOCK,DATE,OPEN,HIGH,LOW,CLOSE,VOLUME) VALUES ("+input+","+res[i]+",";
 			}else if(i%6 == 5){
-				outputHTML = outputHTML + res[i]+"</span><br>";
+				outputHTML = outputHTML + res[i]+");</span><br>";
 			}else{
 				outputHTML = outputHTML+res[i]+",";
 			}
@@ -54,11 +64,3 @@ function getCSV(){
 
 	return false;
 }
-
-function copyAll(element){
-  var text = $(element).clone().find('br').prepend('\r\n').end().text()
-  element = $('<textarea>').appendTo('body').val(text).select()
-  document.execCommand('copy')
-  element.remove()
-}
-
